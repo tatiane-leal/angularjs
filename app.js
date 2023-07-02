40da1e0e5846f2ea697576b3d2a55b0c36209400
Tatiane Sala <tatianedleal@gmail.com>
2023-07-01 21:53:59 -0400

    initial commit for index

---
+++ app.js
@@ -0,0 +1,29 @@
+// MODULE
+var angularApp = angular.module("angularApp", []);
+
+// CONTROLLERS
+angularApp.controller("mainController", [
+  "$scope",
+  "$filter",
+  function ($scope, $filter) {
+    $scope.handle = "";
+
+    $scope.handleLowerCase = function () {
+      console.log("triggered");
+      return $filter("lowercase")($scope.handle);
+    };
+
+    $scope.characters = 5;
+    $scope.rules = [
+      {
+        rulename: "Must be 5 characters",
+      },
+      {
+        rulename: "Must not be used elsewhere",
+      },
+      {
+        rulename: "Must be cool",
+      },
+    ];
+  },
+]);
